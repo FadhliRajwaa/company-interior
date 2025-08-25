9384151e1e6daf3722fdc816f212a65d094ba171
FadhliRajwaa <fadhlirajwaarahmana@gmail.com>
2025-08-25 20:10:01 +0700

    fix render

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+
+# Jalankan storage:link agar symlink public/storage tersedia
+php artisan storage:link || true
+
 # Jalankan migrate dan seed hanya jika database bisa diakses
 if php artisan migrate:status > /dev/null 2>&1; then
   php artisan migrate:fresh --seed || true
